bee9879a39b3cb3155625b3553b23da469a4ccad
Hassan <hassaankhaliq45@gmail.com>
2026-05-26 18:20:59 +0500

    fix re intialize variable bug

--- backend/src/controllers/dashboardController.js
+++ backend/src/controllers/dashboardController.js
@@ -56,8 +56,6 @@ export const getStats = async (req, res) => {
         plan: clientInfo.plan,
         plan_expires_at: clientInfo.plan_expires_at,
         days_left: daysLeft,
-        in_grace_period: inGracePeriod,
-        grace_days_left: graceDaysLeft,
       }
     });
 
